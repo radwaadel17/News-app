5c8702c99c803f909f87c02a23d0b13b7c1396fc
radwaadel17 <radwaadel959@gmail.com>
2024-07-29 20:56:32 +0300

    Finish getGenralNews method

--- news_app_project/lib/Services/NewsService.dart
+++ news_app_project/lib/Services/NewsService.dart
@@ -1,28 +1,26 @@
 import 'package:dio/dio.dart';
 import 'package:news_app_project/models/ArticlesClass.dart';
-class NewsServices{
-  final Dio dio ; 
+class NewsServices {
+  final Dio dio;
   const NewsServices(this.dio);
-  void getGenralNews() async {
-    Response response =  await dio.get('https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=e16edadcc5a34f43bf21fa9999da78e8');
-    Map <String , dynamic > jsonData = response.data;
+  Future<List<ArticlesModel>> getGenralNews() async {
+    Response response = await dio.get(
+        'https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=e16edadcc5a34f43bf21fa9999da78e8');
+    Map<String, dynamic> jsonData = response.data;
     List<dynamic> articles = jsonData['articles'];
     List<ArticlesModel> articlesList = getList(articles);
-    print(articlesList);
-    
+    return articlesList ;
+    //  print(articlesList);
   }
-
-} 
-    List<ArticlesModel> getList(List<dynamic> articles){
-     List<ArticlesModel> articlesList = [] ;
-     for(int i = 0 ; i < articles.length ; i++){
-      articlesList.add(ArticlesModel(
-      img: articles[i]['urlToImage'] , 
-
+}
+List<ArticlesModel> getList(List<dynamic> articles) {
+  List<ArticlesModel> articlesList = [];
+  for (int i = 0; i < articles.length; i++) {
+    articlesList.add(ArticlesModel(
+      img: articles[i]['urlToImage'],
       title: articles[i]['title'],
-
       subTitle: articles[i]['description'],
-       ));
-     }
-     return articlesList ;
-    }     
+    ));
+  }
+  return articlesList;
+}
